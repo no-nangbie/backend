d8b14bc4ec72c73ac1c1ce009a38cfc4aa2bbc05
Tizesin <tlsalswns0o@gmail.com>
2024-09-02 23:51:19 +0900

    init backend

---
+++ src/main/java/com/nonangbie/nonangbieApplication.java
@@ -0,0 +1,12 @@
+package com.nonangbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class nonangbieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(nonangbieApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nonangbie/nonangbieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonangbie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class nonangbieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
