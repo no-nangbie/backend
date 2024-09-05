af048447d1ada18ab9299d16e5cf863e4171db6c
wooil6 <wooilkf@naver.com>
2024-09-05 10:26:34 +0900

    test

---
+++ src/main/java/com/nonangbie/food/dto/FoodDto.java
@@ -0,0 +1,48 @@
+package com.nonangbie.food.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import com.nonangbie.food.entity.Food.FoodCategory;
+import com.nonangbie.food.entity.Food.FoodStatus;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+public class FoodDto {
+
+    @Getter
+    @AllArgsConstructor
+    public static class Post {
+        @NotBlank
+        private String foodName;
+
+        @NotBlank
+        private FoodCategory foodCategory;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Patch {
+        @Setter
+        private long foodId;
+
+        @NotBlank
+        private String foodName;
+
+        @NotBlank
+        private FoodCategory foodCategory;
+
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Response {
+        private long foodId;
+        private String foodName;
+        private FoodCategory foodCategory;
+        private FoodStatus foodStatus;
+    }
+}
