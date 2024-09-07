7719bd1838c2db9a5c6738ac7b808bab6a219e08
wooil6 <wooilkf@naver.com>
2024-09-07 22:42:52 +0900

    fix: #49 Member_Food TEST

--- src/main/java/com/nonangbie/memberFood/repository/MemberFoodRepository.java
+++ src/main/java/com/nonangbie/memberFood/repository/MemberFoodRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface MemberFoodRepository extends JpaRepository<MemberFood, Long> {
 
     List<MemberFood> findByFood_FoodNameContainingOrMemoContaining(String foodNameKeyword, String memoKeyword);
+
+    List<MemberFood> findByFood_FoodId(Long foodId);
 }
--- src/main/java/com/nonangbie/memberFood/service/MemberFoodService.java
+++ src/main/java/com/nonangbie/memberFood/service/MemberFoodService.java
@@ -3,13 +3,13 @@ package com.nonangbie.memberFood.service;
 import com.nonangbie.exception.BusinessLogicException;
 import com.nonangbie.exception.ExceptionCode;
 import com.nonangbie.food.entity.Food;
+import com.nonangbie.food.repository.FoodRepository;
 import com.nonangbie.food.service.FoodService;
 import com.nonangbie.member.entity.Member;
 import com.nonangbie.memberFood.entity.MemberFood;
 import com.nonangbie.memberFood.repository.MemberFoodRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -18,21 +18,23 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class MemberFoodService {
     private final MemberFoodRepository memberFoodRepository;
-    private final FoodService foodService;
+    private final FoodRepository foodRepository;
 
     public MemberFoodService(MemberFoodRepository memberFoodRepository,
-                             FoodService foodService) {
+                             FoodRepository foodRepository) {
         this.memberFoodRepository = memberFoodRepository;
-        this.foodService = foodService;
+        this.foodRepository = foodRepository;
     }
 
     public MemberFood createMemberFood(MemberFood memberFood) {
-        Food findFood = foodService.findVerifiedFood(memberFood.getFood().getFoodId());
+        Food findFood = foodRepository.findByFoodName(memberFood.getFood().getFoodName())
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.FOOD_NOT_FOUND));
         memberFood.setFood(findFood);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
@@ -40,9 +42,9 @@ public class MemberFoodService {
         LocalDate currentDate = LocalDate.now();
         long remainingShelfLife = ChronoUnit.DAYS.between(currentDate, expirationDate);
 
-        if (remainingShelfLife > 0 && remainingShelfLife <= 3) {
+        if (remainingShelfLife >= 0 && remainingShelfLife <= 3) {
             memberFood.setMemberFoodStatus(MemberFood.MemberFoodStatus.Approaching_Expiry);
-        } else if (remainingShelfLife <= 0) {
+        } else if (remainingShelfLife < 0) {
             memberFood.setMemberFoodStatus(MemberFood.MemberFoodStatus.Near_Expiry);
         } else {
             memberFood.setMemberFoodStatus(MemberFood.MemberFoodStatus.Fresh);
@@ -54,6 +56,13 @@ public class MemberFoodService {
     public MemberFood updateMemberFood(MemberFood memberFood) {
         MemberFood findMemberFood = findVerifiedMemberFood(memberFood.getMemberFoodId());
 
+        Optional.ofNullable(memberFood.getFood())
+                .map(Food::getFoodName)
+                .ifPresent(foodName -> {
+                    Food findFood = foodRepository.findByFoodName(foodName)
+                            .orElseThrow(() -> new BusinessLogicException(ExceptionCode.FOOD_NOT_FOUND));
+                    findMemberFood.setFood(findFood);
+                });
         Optional.ofNullable(memberFood.getFoodCategory())
                 .ifPresent(foodCategory -> findMemberFood.setFoodCategory(foodCategory));
         Optional.ofNullable(memberFood.getExpirationDate())
@@ -66,9 +75,9 @@ public class MemberFoodService {
         LocalDate currentDate = LocalDate.now();
         long remainingShelfLife = ChronoUnit.DAYS.between(currentDate, expirationDate);
 
-        if (remainingShelfLife > 0 && remainingShelfLife <= 3) {
+        if (remainingShelfLife >= 0 && remainingShelfLife <= 3) {
             memberFood.setMemberFoodStatus(MemberFood.MemberFoodStatus.Approaching_Expiry);
-        } else if (remainingShelfLife <= 0) {
+        } else if (remainingShelfLife < 0) {
             memberFood.setMemberFoodStatus(MemberFood.MemberFoodStatus.Near_Expiry);
         } else {
             memberFood.setMemberFoodStatus(MemberFood.MemberFoodStatus.Fresh);
@@ -89,6 +98,17 @@ public class MemberFoodService {
         return memberFoodRepository.findByFood_FoodNameContainingOrMemoContaining(keyword, keyword);
     }
 
+    // 카테고리 내에서 검색
+    public Page<MemberFood> searchByCategory(Pageable pageable, String keyword, Food.FoodCategory foodCategory) {
+        Page<Food> foods = foodRepository.findByFoodNameContainingAndFoodCategory(pageable, keyword, foodCategory);
+
+        List<MemberFood> memberFoods = foods.stream()
+                .flatMap(food -> memberFoodRepository.findByFood_FoodId(food.getFoodId()).stream())
+                .collect(Collectors.toList());
+
+
+        return new PageImpl<>(memberFoods, pageable, memberFoods.size());
+    }
 
     public void deleteMemberFood(long memberFoodId) {
         MemberFood findMemberFood = findMemberFood(memberFoodId);
