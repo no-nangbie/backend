3395c3b03180a7c2bed9b74f44a3b0d5850d0aac
Lifesheep <scientist5@naver.com>
2024-09-09 13:54:59 +0900

    asdf

--- src/main/java/com/nonangbie/utils/ExtractMemberEmail.java
+++ src/main/java/com/nonangbie/utils/ExtractMemberEmail.java
@@ -4,13 +4,12 @@ import com.nonangbie.exception.BusinessLogicException;
 import com.nonangbie.exception.ExceptionCode;
 import com.nonangbie.member.entity.Member;
 import com.nonangbie.member.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 
 public abstract class ExtractMemberEmail {
 
     public Member extractMemberFromAuthentication(Authentication authentication,
-                                                   MemberRepository memberRepository) {
+                                                  MemberRepository memberRepository)  {
 
         String username = (String) authentication.getPrincipal();
 
