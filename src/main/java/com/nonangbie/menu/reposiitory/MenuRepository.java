95c5c2ddfd0546d381e5ac9239788df2a3325c13
wooil6 <wooilkf@naver.com>
2024-09-11 16:11:39 +0900

    fix: #75 메뉴 검색 수정

--- src/main/java/com/nonangbie/menu/reposiitory/MenuRepository.java
+++ src/main/java/com/nonangbie/menu/reposiitory/MenuRepository.java
@@ -1,5 +1,6 @@
 package com.nonangbie.menu.reposiitory;
 
+import com.nonangbie.memberFood.entity.MemberFood;
 import com.nonangbie.menu.entity.Menu;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,11 +8,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
     Page<Menu> findByMenuCategory(Pageable pageable, Menu.MenuCategory menuCategory);
+
+    List<Menu> findByMenuTitleContainingOrMenuDescriptionContaining(String menuTitleKeyword, String menuDescriptionKeyword);
 
     @Query("SELECT m FROM Menu m WHERE (m.menuTitle LIKE %:keyword%) AND m.menuCategory = :menuCategory")
     Page<Menu> searchByMenuTitleAndCategory(Pageable pageable, @Param("keyword") String keyword, @Param("menuCategory") Menu.MenuCategory menuCategory);
