ddb4084c5b94f7a79605c98a63623bcaab962064
Tizesin <tlsalswns0o@gmail.com>
2024-09-18 13:38:10 +0900

    feat:#31 board 검색 및 페이지네이션 기능 수정

--- src/main/java/com/nonangbie/board/repository/BoardRepository.java
+++ src/main/java/com/nonangbie/board/repository/BoardRepository.java
@@ -4,17 +4,30 @@ import com.nonangbie.board.entity.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    Page<Board> findAllByMenuCategory(Board.MenuCategory type, Pageable pageable);
-
-    Page<Board> findAllByMenuCategoryAndBoardIdIn(Board.MenuCategory type, List<Long> ids, Pageable pageable);
-
-    Page<Board> findAll(Pageable pageable);
-
-    Page<Board> findAllByBoardIdIn(List<Long> ids, Pageable pageable);
+    @Query("SELECT b FROM Board b WHERE "
+            + "(:menuCategory IS NULL OR b.menuCategory = :menuCategory) AND "
+            + "(:keyword IS NULL OR b.title LIKE %:keyword%) AND "
+            + "(:ids IS NULL OR b.boardId IN :ids)")
+    Page<Board> findBoards(
+            @Param("menuCategory") Board.MenuCategory menuCategory,
+            @Param("keyword") String keyword,
+            @Param("ids") List<Long> ids,
+            Pageable pageable
+    );
+    @Query("SELECT b FROM Board b WHERE "
+            + "(:menuCategory IS NULL OR b.menuCategory = :menuCategory) AND "
+            + "(:ids IS NULL OR b.boardId IN :ids)")
+    Page<Board> findBoardsNoKeyword(
+            @Param("menuCategory") Board.MenuCategory menuCategory,
+            @Param("ids") List<Long> ids,
+            Pageable pageable
+    );
 }
