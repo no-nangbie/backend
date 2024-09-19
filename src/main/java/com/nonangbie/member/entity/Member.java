473a512cc029b66049678858ab0ac649053e803d
Lifesheep <scientist5@naver.com>
2024-09-19 12:05:00 +0900

    feat:#115 닉네임 변경 기능 구현

--- src/main/java/com/nonangbie/member/entity/Member.java
+++ src/main/java/com/nonangbie/member/entity/Member.java
@@ -29,6 +29,7 @@ public class Member {
     @Column(name = "password", length = 255, nullable = false)
     private String password;
 
+    @Setter
     @Column(name = "nickname", length = 255, nullable = false)
     private String nickname;
 
